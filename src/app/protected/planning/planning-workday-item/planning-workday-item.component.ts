8326ba60c1bfc24423d16f94f9c4c34ae38a3f39
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2023-01-09 09:11:12 +0100

    modify planning components delete feature on workday with bi-directional annotation

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'al-planning-workday-item',
@@ -10,7 +10,9 @@ export class PlanningWorkdayItemComponent implements OnChanges {
     @Input() dueDate: string;
     @Input() doneTasks: number | string;
     @Input() remainingTasks: number | string;
-     
+    
+    @Output() workdayRemoved = new EventEmitter<string>();
+
     ngOnChanges(changes: SimpleChanges) {
      for (const propName in changes) {
       this.update(propName, changes[propName].currentValue);
@@ -38,6 +40,10 @@ export class PlanningWorkdayItemComponent implements OnChanges {
        break;
       }
      }
+    }
+
+    removeWorkday(dueDate: string) {
+      this.workdayRemoved.emit(dueDate)
     }
      
    }
--- src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -10,19 +10,26 @@ import { delay } from 'rxjs/operators';
   ]
 })
 export class PlanningWorkdayListComponent implements OnInit {
-  
+   
+  workdays: { dueDate: string, doneTasks: number, remainingTasks: number }[];
   workdays$: Observable<{ dueDate: string, doneTasks: number, remainingTasks: number }[]>;
    
-  constructor() { }
-   
-  ngOnInit() {
-   
-   this.workdays$ = of([
-    { dueDate: 'Vendredi', doneTasks: 1, remainingTasks: 3 },
-    { dueDate: 'Mardi', doneTasks: 1, remainingTasks: 2 },
-    { dueDate: 'Mercredi', doneTasks: 1, remainingTasks: 0 }
-   ]).pipe(delay(1000));
-   
-  }
-   
+ ngOnInit() {
+  this.workdays = [
+   { dueDate: 'Lundi', doneTasks: 1, remainingTasks: 0 },
+   { dueDate: 'Mardi', doneTasks: 0, remainingTasks: 2 },
+   { dueDate: 'Mercredi', doneTasks: 0, remainingTasks: 1 }
+  ];
+     
+  this.workdays$ = of(this.workdays).pipe(delay(1000));
  }
+  
+ // Ajoutez notre gestionnaire d’événement :
+ onWorkdayRemoved(dueDate: string) {
+  this.workdays = this.workdays.filter(workday => 
+   !dueDate.includes(workday.dueDate)
+  );
+  this.workdays$ = of(this.workdays);
+ }
+  
+}
