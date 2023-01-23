a409aa61b36c79e3e15f783711d09d4b0c589b8b
thibautDevWorld <thibaut.lartigue.pro@gmail.com>
2023-01-23 10:43:44 +0100

    implement profil component

--- src/app/protected/profil/profil/profil.component.ts
+++ src/app/protected/profil/profil/profil.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'al-profil',
@@ -6,6 +9,39 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit {
+   profilForm: FormGroup;
+   user: User|null;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+
+    this.user = this.authService.currentUser;
+
+    this.profilForm = this.fb.group({
+      'name': [this.user?.name, [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+        Validators.pattern('^[a-zA-Z0-9_-]*$')
+      ]],
+      'avatar': [this.user?.avatar, [
+        Validators.pattern('https?://.+')
+      ]]
+    })
+  }
+  
+  get name() { return this.profilForm.get('name') as FormControl }
+  get avatar() { return this.profilForm.get('avatar') as FormControl }
+
+
+
+   submit() {
+    if(this.user) {
+      this.user.name = this.name?.value;
+      this.user.avatar = this.avatar?.value;
+      this.authService.updateUserState(this.user).subscribe();
+    }
+   }
 }
